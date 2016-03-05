7accc7b22a0a1c3ea3239ffbe75ae037b4c83482
Oleksii <ofadress@gmail.com>
2016-03-06 00:58:57 +0200

    add react task

---
+++ tasks/react.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var Task = require( './task' );
+var react = require( './' );
+
+var React = Task.createChild();
+
+React.prototype.main = function( stream ) {
+  return stream.pipe( react() );
+};
+
+module.exports = React;
