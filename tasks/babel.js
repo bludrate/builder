9fc86faea623f58e4dcf661878b8f4377f71cf80
Oleksii <ofadress@gmail.com>
2016-03-06 02:06:06 +0200

    fix babel task

--- tasks/babel.js
+++ tasks/babel.js
@@ -7,8 +7,7 @@ var extend = require( '../util/extend' );
 var Babel = Task.createChild();
 
 Babel.prototype.main = function( stream ) {
-  var opts = extend( { presets: ['es2015'] }, this.config.opts );
-  return stream.pipe( babel( opts ) );
+  return stream.pipe( babel( this.config.opts ) );
 };
 
 module.exports = Babel;
